a0d914ce85ca68deaa63c485b7360626fb5c0128
zyao89 <zyao89@gmail.com>
2018-09-21 14:54:09 +0800

    :sparkles: 新增全局路径配置 process.env.LOGGER_ROOT , 以及增加 logger 域名

--- index.js
+++ index.js
@@ -2,4 +2,24 @@
 
 const Logger = require('./lib');
 
+const loggerMap = new Map();
+
+/**
+ * 获取一个实例
+ * @param {String|Symbol} name name
+ * @return {Logger} instance
+ */
+Logger.instance = function(name) {
+    if (name) {
+        if (loggerMap.has(name)) {
+            return loggerMap.get(name);
+        }
+    }
+    const log = new Logger(name);
+    if (name) {
+        loggerMap.set(name, log);
+    }
+    return log;
+};
+
 module.exports = Logger;
